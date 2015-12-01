26e1f67cb5d8a8e5d8e28f41989fe790b220dead
Richard Ding <rmding95@uw.edu>
2015-11-30 22:50:15 -0800

    added basic form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("EventFinderApp", ['ngSanitize', 'ui.router'])
+angular.module("EventFinderApp", ['ngSanitize', 'ui.router', 'ui.bootstrap'])
     .config(function($stateProvider){
         $stateProvider
             //home page
@@ -19,11 +19,51 @@ angular.module("EventFinderApp", ['ngSanitize', 'ui.router'])
     .controller("HomeCtrl", ["$scope", function($scope) {
 
         var map = L.map('map-container').setView([37.50, -97.00], 4);
-        L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-            maxZoom: 15,
+        L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGhhbjIwNiIsImEiOiJjaWZzeWE4c2QwZDAzdHRseWRkMXR2b2Y5In0.Gbh1YncNoaD5W4zylMfNTw", {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 100,
             minZoom: 3,
             id: "mapbox.light",
             accessToken: "pk.eyJ1IjoiZGhhbjIwNiIsImEiOiJjaWZzeWE4c2QwZDAzdHRseWRkMXR2b2Y5In0.Gbh1YncNoaD5W4zylMfNTw"
         }).addTo(map);
+
+        //TODO: MAKE SEPARATE HANDLERS FOR THE 2 DATEPICKERS, MAKE IT LOOK PRETTY, SELECT ONE CHECKBOX DISABLES OTHER 
+        $scope.today = new Date();
+        $scope.dateStart = new Date();
+        $scope.dateEnd = new Date();
+
+        var dd = $scope.today.getDate();
+        var mm = $scope.today.getMonth()+1; //January is 0!
+        var yyyy = $scope.today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mm='0'+mm
+        } 
+
+        $scope.today = mm+'/'+dd+'/'+yyyy;
+        console.log($scope.today);
+
+        $scope.open = function($event) {
+            $scope.status.isOpen = true;
+            console.log($scope.dateStart);
+            console.log($scope.dateEnd);
+        }
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.status = {
+            opened: false
+        };
+
+        $scope.format = 'shortDate';
+
+
 
     }])
