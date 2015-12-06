63dc1911bba1aac78da4d743891197e8945dc85e
DereK Han <derekhan206@gmail.com>
2015-12-05 17:41:53 -0800

    Fixes marker plotting bug

--- js/app.js
+++ js/app.js
@@ -61,7 +61,6 @@ angular.module("EventFinderApp", ['ngSanitize', 'ui.router', 'ui.bootstrap'])
                     var date = new Date(data.datetime_local);
                     var month = date.getMonth() + 1;
                     marker.bindPopup("<p class='eventTitle'>" + data.title + "</p>" + month + "/" + date.getDate() + "/" + date.getFullYear()  + "<br>" + data.venue.name + "<br><a href='https://maps.google.com?daddr=" + lat + "," + lon + "'>Get directions!</a>" + "<br><a href='" + data.url + "'>Seatgeek Listing</a>")
-                    marker.addTo(map);
                     marker.addTo(typeLayers[data.type]);
                 });
                 layerControl = L.control.layers(null, typeLayers);
