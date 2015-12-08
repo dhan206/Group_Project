5989dfc0358452a5c8f7f1821af1a5688ffcdb45
Kendall Reonal <kreonal@uw.edu>
2015-12-08 15:57:10 -0800

    military time to standard

--- js/app.js
+++ js/app.js
@@ -77,10 +77,47 @@ angular.module("EventFinderApp", ['ngSanitize', 'ui.router', 'ui.bootstrap'])
                         artist.push(" " + performance.artist.displayName);
                     });
 
+                    var milTime = data.start.time;
+                    var standardTime;
+                    if (milTime == null) {
+                        standardTime = 'Not specified';
+                    } else {
+                        standardTime = milToStandard(milTime);
+                    }
+                    function milToStandard(value) {
+                        if (value !== null && value !== undefined){ //If value is passed in
+                            if(value.indexOf('AM') > -1 || value.indexOf('PM') > -1){ //If time is already in standard time then don't format.
+                              return value;
+                            }
+                            else {
+                              if(value.length == 8){ //If value is the expected length for military time then process to standard time.
+                                var hour = value.substring ( 0,2 ); //Extract hour
+                                var minutes = value.substring ( 3,5 ); //Extract minutes
+                                var identifier = 'AM'; //Initialize AM PM identifier
+
+                                if(hour == 12){ //If hour is 12 then should set AM PM identifier to PM
+                                  identifier = 'PM';
+                                }
+                                if(hour == 0){ //If hour is 0 then set to 12 for standard time 12 AM
+                                  hour=12;
+                                }
+                                if(hour > 12){ //If hour is greater than 12 then convert to standard 12 hour format and set the AM PM identifier to PM
+                                  hour = hour - 12;
+                                  identifier='PM';
+                                }
+                                return hour + ':' + minutes + ' ' + identifier; //Return the constructed standard time
+                              }
+                              else { //If value is not the expected length than just return the value as is
+                                return value;
+                              }
+                            }
+                        }
+                    };
+
                     // TODO: Add spotify widget to map pop-ups, using the first artist in the artist array
                     // TODO: as the search parameter.
-                    
-                    marker.bindPopup("<p class='eventTitle'>" + data.displayName + "</p><p class='artists'> Artist(s): " + artist.toString() + "</p> Event Date: " + data.start.date + "<br> Venue Name: " + data.venue.displayName + "<br><a href='https://maps.google.com?daddr=" + lat + "," + lon + "'target='_blank'>Get directions!</a>" + "<br><a href='" + data.uri + "'target='_blank'>Link to event page</a>" + "<br><iframe src='https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>");
+                                 
+                    marker.bindPopup("<p class='eventTitle'>" + data.displayName + "</p><p class='artists'> Artist(s): " + artist.toString() + "</p> Event Date: " + data.start.date + "<br> Venue Name: " + data.venue.displayName + "<br><a href='https://maps.google.com?daddr=" + lat + "," + lon + "'target='_blank'>Get directions!</a>" + "<br><a href='" + data.uri + "'target='_blank'>Link to event page</a>");
                     marker.addTo(typeLayers[data.type]);
                 });
             
